b9da0b0328b1c9b30599db213236b12a2b3b1131
luizhcb0 <luizhcb0@gmail.com>
2017-10-08 00:56:38 -0300

    Visual

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,14 @@
 class Device < ApplicationRecord
+  has_many :levels
+  belongs_to :user
+  
+  enum position: %w(top bottom)
+  
+  def self.get_top_devices
+    where(position: "top")
+  end
+  
+  def self.get_bottom_devices
+    where(position: "bottom")
+  end
 end
