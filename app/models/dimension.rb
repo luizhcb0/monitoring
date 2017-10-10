48295546e0066be0e81d42af74eb77fe7e40ebe7
luizhcb0 <luizhcb0@gmail.com>
2017-10-10 05:23:03 -0300

    Created: Dimensions Model

---
+++ app/models/dimension.rb
@@ -0,0 +1,30 @@
+class Dimension < ApplicationRecord
+  belongs_to :device
+  
+  after_create :set_volume
+  
+  def volume
+    self[:x] * self[:y] * self[:z] * 1000
+  end
+  
+  def self.get_tank_volume(device_id)
+    tank = where(device_id: device_id)
+    tank = tank.first
+    volume = tank.x * tank.y * tank.z * 1000
+    return volume
+  end
+  
+  def self.get_all_volumes
+    tanks = Array.new
+    Device.all.each do |device|
+      tanks <<  where(device_id: device.id)
+    end
+    return tanks
+  end
+  
+  private
+    def set_volume
+      self.update_attributes(volume: self[:x] * self[:y] * self[:z] * 1000)
+    end
+  
+end
