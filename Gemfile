5ff83c89c6f016184bca0a7a22e3b8f4b4bfec81
luizhcb0 <luizhcb0@gmail.com>
2017-11-27 22:37:02 -0200

    Added Gem:  bootstrap-toggle-rails

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
 gem 'bootstrap-sass'
+gem 'bootstrap-toggle-rails'
 gem "font-awesome-rails"
 gem 'font-awesome-sass'
 gem 'jquery_mask_rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require bootstrap-toggle
 //= require jquery-readyselector
 //= require jquery.mask
 //= require highstock
