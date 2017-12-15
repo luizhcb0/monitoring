5e03467f37d365d21c6293f4823def6a11049fdd
luizhcb0 <luizhcb0@gmail.com>
2017-12-15 19:00:05 -0200

    Created: Time between critical levels

--- app/mailers/default_mailer.rb
+++ app/mailers/default_mailer.rb
@@ -5,9 +5,22 @@ class DefaultMailer < ApplicationMailer
     @user = user
     @device = device
     @percentage = percentage
+    @tbcl = @device.time_between_cl.to_i
+    @last_cl = Time.at(@device.last_cl.to_i - @device.time_between_cl.to_i)
+
+    if @tbcl < 60
+      @text = "O reservatório levou #{@tbcl} segundos para chegar ao nível crítico."
+    elsif @tbcl >= 60 && @tbcl < 3600
+      @text = "O reservatório levou #{(@tbcl/60).round} minutos para chegar ao nível crítico."
+    elsif @tbcl >= 3600 && @tbcl < 86400
+      @text = "O reservatório levou #{(@tbcl/3600).round} horas para chegar ao nível crítico."
+    elsif @tbcl >= 86400
+      @text = "O reservatório levou #{(@tbcl/86400).round} dias para chegar ao nível crítico."
+    end
+
     mail(to: @user.email, subject: 'Alerta')
   end
-  
+
   def normal_email(user, device, percentage)
     @user = user
     @device = device
