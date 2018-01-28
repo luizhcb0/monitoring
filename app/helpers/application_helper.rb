30d2015767a84b10c00037552b150a2e3a779608
luizhcb0 <luizhcb0@gmail.com>
2018-01-28 14:25:17 -0200

    Added: Theme separator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def user_array
     user_array =  User.all.map { |user| [user.name, user.id] }
   end
+
+  def current_theme
+    # current_theme = current_user.theme || 'default'
+    current_theme = 'white_theme/application'
+  end
 end
