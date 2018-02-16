dbcbc30edd0d59c95942ec99f79b0cd2be21c363
luizhcb0 <luizhcb0@gmail.com>
2018-02-16 16:06:05 -0200

    Changes on Defaut Mailer
    Added callback to update attributes after sending email

--- app/mailers/default_mailer.rb
+++ app/mailers/default_mailer.rb
@@ -1,32 +1,37 @@
 class DefaultMailer < ApplicationMailer
   default from: '"LCA Systems" <atendimento@lcasystems.com.br>'
 
+  after_action :set_last_critical_level, only: :alert_email
+
   def alert_email(user, device, level)
     @user = user
     @device = device
+    @level = level
     @percentage = level.percentage
     @created_at = level.created_at
-    @tbcl = @device.time_between_cl.to_i
-    @last_cl = Time.at(@device.last_cl.to_i - @device.time_between_cl.to_i)
-
-    if @tbcl < 60
-      @text = "O reservatório levou #{@tbcl} segundos para chegar ao nível crítico."
-    elsif @tbcl >= 60 && @tbcl < 3600
-      @text = "O reservatório levou #{(@tbcl/60).round} minutos para chegar ao nível crítico."
-    elsif @tbcl >= 3600 && @tbcl < 86400
-      @text = "O reservatório levou #{(@tbcl/3600).round} horas para chegar ao nível crítico."
-    elsif @tbcl >= 86400
-      @text = "O reservatório levou #{(@tbcl/86400).round} dias para chegar ao nível crítico."
-    end
-
+    @last_cl = device.last_cl
     mail(to: @user.email, subject: 'Alerta')
   end
 
   def normal_email(user, device, level)
     @user = user
     @device = device
+    @level = level
     @percentage = level.percentage
     @created_at = level.created_at
+    @last_cl = device.last_cl
+    @tbcn = level.created_at.to_i - device.last_cl.to_i
+
+    if @tbcn < 60
+      @text = "O reservatório levou #{@tbcn} segundo(s) para reestabelecer as condições normais."
+    elsif @tbcn >= 60 && @tbcn < 3600
+      @text = "O reservatório levou #{(@tbcn/60).round} minuto(s) para reestabelecer as condições normais."
+    elsif @tbcn >= 3600 && @tbcn < 86400
+      @text = "O reservatório levou aproximadamente #{(@tbcn/3600).round} hora(s) para reestabelecer as condições normais."
+    elsif @tbcn >= 86400
+      @text = "O reservatório levou aproximadamente #{(@tbcn/86400).round} dia(s) para reestabelecer as condições normais."
+    end
+
     mail(to: @user.email, subject: 'Condições normalizadas')
   end
 
@@ -34,4 +39,9 @@ class DefaultMailer < ApplicationMailer
     @contact = contact
     mail(to: "atendimento@lcasystems.com.br", from: "#{contact.name} <#{contact.email}>", subject: contact.subject)
   end
+
+  private
+    def set_last_critical_level
+      @device.update_attributes(last_cl: @level.created_at)
+    end
 end
