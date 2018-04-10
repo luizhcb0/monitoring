843d409c0fa32be467f533918c0cccc7e03a16b7
luizhcb0 <luizhcb0@gmail.com>
2018-04-09 23:35:49 -0300

    Changed: Thermometer View

--- app/assets/javascripts/dark_theme/weather_monitoring.js
+++ app/assets/javascripts/dark_theme/weather_monitoring.js
@@ -1,6 +1,8 @@
 $updateRate = 300000;
 $devices = {};
-$devices_temperature = {}
+$devices_temperature_integer = {}
+$devices_temperature_decimal = {}
+$devices_temperature_rainbow = {}
 $devices_humidity = {}
 $devices_luminosity = {}
 $devices_atm_pressure = {}
@@ -27,7 +29,9 @@ function cacheInfo() {
     success: function(response){
       for (i = 0; i < response.length; i++) {
         $devices[i] = $('#device-'+response[i][0].id)
-        $devices_temperature[i] = $('#device-'+response[i][0].id+"-temperature")
+        $devices_temperature_integer[i] = $('#device-'+response[i][0].id+"-temperature-integer")
+        $devices_temperature_decimal[i] = $('#device-'+response[i][0].id+"-temperature-decimal")
+        $devices_temperature_rainbow[i] = $('#device-'+response[i][0].id+"-temp-rainbow")
         $devices_humidity[i] = $('#device-'+response[i][0].id+"-humidity")
         $devices_luminosity[i] = $('#device-'+response[i][0].id+"-luminosity")
         $devices_atm_pressure[i] = $('#device-'+response[i][0].id+"-atm_pressure")
@@ -44,12 +48,48 @@ function getInfo() {
     dataType: "json",
     success: function(response){
       for (i = 0; i < response.length; i++) {
-        $devices_temperature[i].html(response[i][1].data+"ºC")  //Temperature
+        $temp = response[i][1].data.toFixed(1);
+        $intPart = ($temp+"").split(".")[0];
+        $decPart = ($temp+"").split(".")[1];
+        $devices_temperature_integer[i].html($intPart)  //Temperature
+        $devices_temperature_decimal[i].html($decPart)  //Temperature
+        changeTemperatureColor($devices_temperature_rainbow[i], $intPart) //Temperature
         $devices_humidity[i].html(response[i][2].data+"%")  //Humidity
         $devices_luminosity[i].html(response[i][3].data+"")  //Luminosity
         $devices_atm_pressure[i].html(response[i][4].data+"hPa")  //Atm Pressure
       }
     }
   });
+  return false;
+}
+
+function changeTemperatureColor($object, $temp) {
+  if ($temp <= 10) {
+    $object.css('background', '#5bd5f6');
+  }
+  else if ($temp > 10 && $temp <= 15) {
+    $object.css('background', '#65e0d5');
+  }
+  else if ($temp > 15 && $temp <= 20) {
+    $object.css('background', '#8ff8c5');
+  }
+  else if ($temp > 20 && $temp <= 25) {
+    $object.css('background', '#bfeab5');
+  }
+  else if ($temp > 25 && $temp <= 30) {
+    $object.css('background', '#cfeaab');
+  }
+  else if ($temp > 30 && $temp <= 35) {
+    $object.css('background', '#f6e58b');
+  }
+  else if ($temp > 35 && $temp <= 40) {
+    $object.css('background', '#f6c57b');
+  }
+  else if ($temp > 40 && $temp <= 45) {
+    $object.css('background', '#f6855b');
+  }
+  else if ($temp > 45) {
+    $object.css('background', '#f6655b');
+  }
   return false;
 }
