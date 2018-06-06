3f0171dfdd48e241b332577255d28de064a941a9
luizhcb0 <luizhcb0@gmail.com>
2018-06-06 17:10:40 -0300

    Changed: Model changes for new weather Graphs

--- app/models/humidity.rb
+++ app/models/humidity.rb
@@ -1,3 +1,10 @@
 class Humidity < ApplicationRecord
   belongs_to :device
+
+  def self.get_humidities(device_id)
+    data = Array.new
+    now = DateTime.now
+    data = where(device_id: device_id, created_at: (now - 2.day)..now).order(created_at: :asc)
+    return data
+  end
 end
--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -1,3 +1,10 @@
 class Temperature < ApplicationRecord
   belongs_to :device
+
+  def self.get_temperatures(device_id)
+    data = Array.new
+    now = DateTime.now
+    data = where(device_id: device_id, created_at: (now - 2.day)..now).order(created_at: :asc)
+    return data
+  end
 end
